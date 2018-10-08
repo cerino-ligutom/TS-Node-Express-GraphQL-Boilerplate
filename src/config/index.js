35b5879d61159f4f6bd9ea98c4fbec8639ccdbc6
Cerino O. Ligutom III <zeferinix@gmail.com>
2018-10-08 12:14:12 +0800

    Refactored environment

--- src/config/index.js
+++ src/config/index.js
@@ -2,11 +2,29 @@ require('dotenv').config();
 
 const appRoot = require('app-root-path');
 
-export const environment = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  host: process.env.HOST || 'localhost',
-  logDirectory: `${appRoot}/logs`,
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const environment = {
+  development: {
+    NODE_ENV: 'development',
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
+    logDirectory: `${appRoot}/logs`,
+  },
+
+  staging: {
+    NODE_ENV: 'staging',
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
+    logDirectory: `${appRoot}/logs`,
+  },
+
+  production: {
+    NODE_ENV: 'production',
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
+    logDirectory: `${appRoot}/logs`,
+  },
 };
 
-export default environment;
+export default environment[NODE_ENV];
