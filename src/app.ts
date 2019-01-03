b2e618471f824a6a12aaa6abea089c0aa02807b1
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-01-03 19:02:33 +0800

    Added compression and cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
-import { env } from '@EMERE/config/environment';
+import compression from 'compression';
+import cors from 'cors';
 import express from 'express';
+
+import { env } from '@EMERE/config/environment';
 import initRoutes from './routes';
 
 const app = express();
 
 const startApp = async () => {
+  app.use(cors());
+  app.use(express.json());
+  app.use(compression());
+
   app.use('/api', initRoutes());
 
   app.listen(env.PORT, () => {
