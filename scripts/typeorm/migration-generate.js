d040c59d51d25314bd222755c176f075b67fd95f
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-01-06 03:46:35 +0800

    Added migration scripts

---
+++ scripts/typeorm/migration-generate.js
@@ -0,0 +1,18 @@
+const execute = require('child_process').execSync;
+
+const args = process.argv.slice(2);
+let migrationFileName = args[0];
+
+if (migrationFileName && migrationFileName.trim().length) {
+  migrationFileName = migrationFileName.charAt(0).toUpperCase() + migrationFileName.slice(1);
+  console.info('Migration file name:', migrationFileName);
+
+  const command = `ts-node ./node_modules/typeorm/cli.js migration:generate -n ${migrationFileName}`;
+  console.info('Executing command:', command);
+  execute(command, {
+    stdio: 'inherit'
+  });
+} else {
+  console.error('Migration file name not provided.');
+  console.error('Usage: npm run migration:generate <filename>');
+}
