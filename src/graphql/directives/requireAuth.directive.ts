2bbaf65c681aff4d4d292cdbc34bfa1f3d1ba557
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-01-09 19:41:40 +0800

    Initial progress on require auth directive

--- src/graphql/directives/requireAuth.directive.ts
+++ src/graphql/directives/requireAuth.directive.ts
@@ -1,18 +1,57 @@
 import { SchemaDirectiveVisitor, AuthenticationError } from 'apollo-server-express';
-import { defaultFieldResolver, GraphQLField, GraphQLResolveInfo } from 'graphql';
+import { defaultFieldResolver, GraphQLField, GraphQLResolveInfo, GraphQLObjectType } from 'graphql';
 import { IGraphQLContext } from '@EMERE/utils';
 
+interface IRequireAuthDirectiveObjectType extends GraphQLObjectType {
+  _authFieldsWrapped: boolean;
+  _requiredAuthRoles: string[];
+}
+
+interface IRequireAuthDirectiveField extends GraphQLField<any, any> {
+  _requiredAuthRoles: string[];
+}
+
+// https://blog.apollographql.com/reusable-graphql-schema-directives-131fb3a177d1
 export class RequireAuthDirective extends SchemaDirectiveVisitor {
-  public visitFieldDefinition(field: GraphQLField<any, any>) {
-    const { resolve = defaultFieldResolver } = field;
-    field.resolve = async (...args) => {
-      const [, , context, info]: [any, { [key: string ]: any }, IGraphQLContext, GraphQLResolveInfo] = args;
-      if (context.user) {
-        const result = await resolve.apply(this, args);
-        return result;
-      } else {
-        throw new AuthenticationError('Not authenticated. Login required.');
-      }
-    };
+  public visitFieldDefinition(field: GraphQLField<any, any>, details: any) {}
+
+
+  // public visitObject(type: GraphQLObjectType) {}
+
+  private ensureFieldsWrapped(objectType: IRequireAuthDirectiveObjectType) {
+    // Mark the GraphQLObjectType object to avoid re-wrapping:
+    if (objectType._authFieldsWrapped) {
+      return;
+    }
+    objectType._authFieldsWrapped = true;
+
+    const fields = objectType.getFields();
+
+    Object.keys(fields).forEach((fieldName) => {
+      const field = fields[fieldName] as IRequireAuthDirectiveField;
+      const { resolve = defaultFieldResolver } = field;
+
+      field.resolve = async function(...args) {
+        const [, , context, info]: [any, { [key: string]: any }, IGraphQLContext, GraphQLResolveInfo] = args;
+
+        // Get the required Role from the field first, falling back
+        // to the objectType if no Role is required by the field:
+        const requiredRole = field._requiredAuthRoles || objectType._requiredAuthRoles;
+
+        if (!requiredRole) {
+          return resolve.apply(this, args);
+        }
+
+        const { user } = context;
+
+        // const context = args[2];
+        // const user = await getUser(context.headers.authToken);
+        // if (!user.hasRole(requiredRole)) {
+        //   throw new Error('not authorized');
+        // }
+
+        return resolve.apply(this, args);
+      };
+    });
   }
 }
