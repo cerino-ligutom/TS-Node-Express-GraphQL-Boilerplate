80900498d63909bef32c9ffa885529eaad6df772
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-01-15 02:25:10 +0800

    Added error logging on graphql

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,11 +1,12 @@
 import { Express, Request } from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, ApolloError } from 'apollo-server-express';
 import depthLimit from 'graphql-depth-limit';
 import { schema } from './schema';
-import { IGraphQLContext } from '@app/utils';
+import { IGraphQLContext, logger } from '@app/utils';
 import { UserRepository } from '@app/pg/repositories';
 import { GraphQLRouter } from 'src/routes/graphql.routes';
 import { initLoaders } from '../graphql-dataloaders';
+import { env } from '@app/config/environment';
 
 export const initApolloGraphqlServer = (app: Express) => {
   const server = new ApolloServer({
@@ -25,7 +26,19 @@ export const initApolloGraphqlServer = (app: Express) => {
       return graphqlContext;
     },
     validationRules: [depthLimit(10)],
-    introspection: true,
+    formatError: (err: ApolloError) => {
+      // https://www.apollographql.com/docs/apollo-server/features/errors.html#Masking-and-logging-errors
+
+      // We want to log all errors
+      logger.error(err);
+
+      // But not send the exception details to the client when in production
+      if (env.isProduction) {
+        err.extensions!.exception = null;
+      }
+
+      return err;
+    },
   });
 
   app.use(GraphQLRouter);
