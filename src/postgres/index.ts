3aa49e4b3c53145eb2307ba5870f0429844414dc
Cerino O. Ligutom III <zeferinix@gmail.com>
2019-02-27 22:41:22 +0800

    Do not catch error when server fails to connect to db (fail fast)

--- src/postgres/index.ts
+++ src/postgres/index.ts
@@ -9,8 +9,5 @@ export const initDbConnection = async () => {
     .then(async (connection) => {
       console.info(`[TypeORM DB] Connection name: ${connection.name} | isConnected: ${connection.isConnected}`);
       return connection.isConnected;
-    })
-    .catch((err) => {
-      console.error(err);
     });
 };
